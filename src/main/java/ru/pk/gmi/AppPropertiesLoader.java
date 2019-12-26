42f62728da9fbcb30fe52135415f63b6f1f69626
pkprog <1@2.ru>
2019-12-26 16:58:47 +0300

    путь к файлу настройки

--- src/main/java/ru/pk/gmi/AppPropertiesLoader.java
+++ src/main/java/ru/pk/gmi/AppPropertiesLoader.java
@@ -1,6 +1,7 @@
 package ru.pk.gmi;
 
 import ru.pk.gmi.exceptions.ApplicationException;
+import ru.pk.gmi.utils.TypeUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -11,21 +12,28 @@ import java.net.URL;
 import java.util.Properties;
 
 public class AppPropertiesLoader {
-    public static final String FILE = "application.properties.local";
+    public static final String FILE = "application.properties";
+    public static String pathToFolderOfFile;
+
+    public static void init(String pathToFolderOfFile) {
+        AppPropertiesLoader.pathToFolderOfFile = pathToFolderOfFile;
+    }
 
     public static Properties load() {
+        String pathFile = TypeUtils.notEmpty(pathToFolderOfFile) ?
+                (pathToFolderOfFile + "/" + FILE) : (FILE);
         try {
-            return loadFromFile();
+            return loadFromFile(pathFile);
         } catch (URISyntaxException |IOException e) {
             e.printStackTrace();
             throw new ApplicationException("Can not load properties from local file:" + e.getMessage(), e);
         }
     }
 
-    private static Properties loadFromFile() throws IOException, URISyntaxException {
-        URL url = AppPropertiesLoader.class.getClassLoader().getResource(FILE);
+    private static Properties loadFromFile(String filePath) throws IOException, URISyntaxException {
+        URL url = AppPropertiesLoader.class.getClassLoader().getResource(filePath);
         if (url == null) {
-            throw new ApplicationException("Not found file-resource " + FILE);
+            throw new ApplicationException("Not found file-resource:" + filePath);
         }
 
         URI uri = url.toURI();
--- src/main/java/ru/pk/gmi/PeriodicChecker.java
+++ src/main/java/ru/pk/gmi/PeriodicChecker.java
@@ -4,7 +4,12 @@ public class PeriodicChecker {
     private static final int SLEEP_TIME = 60*1*1000; //60 sec
 
     public static void main(String[] params) {
-        Thread th = new Thread(new IpProcessor(SLEEP_TIME));
+        if (params.length > 0) {
+            AppPropertiesLoader.init(params[0]);
+        }
+        IpProcessor ipProcessor = new IpProcessor(SLEEP_TIME);
+
+        Thread th = new Thread(ipProcessor);
         th.start();
         try {
             th.join();
