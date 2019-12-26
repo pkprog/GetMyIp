5f68e531abf4eabc43aea53e1d910060e7b4a7a6
pkprog <1@2.ru>
2019-12-26 17:36:55 +0300

    путь к файлу настройки

--- src/main/java/ru/pk/gmi/AppPropertiesLoader.java
+++ src/main/java/ru/pk/gmi/AppPropertiesLoader.java
@@ -6,9 +6,7 @@ import ru.pk.gmi.utils.TypeUtils;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class AppPropertiesLoader {
@@ -26,7 +24,7 @@ public class AppPropertiesLoader {
             } else {
                 return loadFromResource(DEFAULT_FILE);
             }
-        } catch (URISyntaxException |IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
             throw new ApplicationException("Can not load properties from local file:" + e.getMessage(), e);
         }
@@ -41,17 +39,10 @@ public class AppPropertiesLoader {
         }
     }
 
-    private static Properties loadFromResource(String filePath) throws IOException, URISyntaxException {
-        URL url = AppPropertiesLoader.class.getClassLoader().getResource(filePath);
-        if (url == null) {
-            throw new ApplicationException("Not found file-resource:" + filePath);
-        }
-
-        URI uri = url.toURI();
-
-        try (FileInputStream fis = new FileInputStream(new File(uri))) {
+    private static Properties loadFromResource(String filePath) throws IOException {
+        try (InputStream inputStream = AppPropertiesLoader.class.getClassLoader().getResourceAsStream(filePath)) {
             Properties props = new Properties();
-            props.load(fis);
+            props.load(inputStream);
             return props;
         }
 
